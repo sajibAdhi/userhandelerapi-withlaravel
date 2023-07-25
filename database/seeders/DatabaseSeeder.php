1cd6f5988e3ad478a3b4fc93f1809399d8475a3a
Sajib Adhikary <tosajibadhi@gmail.com>
2023-07-25 17:18:46 +0600

    feat: UserFactory.php
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Admin;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         Admin::factory(1)->create(['email' => 'admin@example.com']);
 
+        User::factory(1000)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
