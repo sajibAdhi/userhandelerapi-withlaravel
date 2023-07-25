72ac0c7169413d88e889d7ae5125060cc1883511
Sajib Adhikary <tosajibadhi@gmail.com>
2023-07-25 18:21:46 +0600

    feat: documentation added to controllers
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -9,10 +9,19 @@ use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
+/**
+ * @group Admin endpoint
+ */
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * GET Users
+     *
+     * Get a list of Users.
+     *
+     * @authenticated
+     *
+     * @response {"data": [{"id": 1,"name": "Dolores Reynolds","division": "C","email": "sawayn.meda@example.net"},]}
      */
     public function index(UsersListRequest $request): AnonymousResourceCollection
     {
