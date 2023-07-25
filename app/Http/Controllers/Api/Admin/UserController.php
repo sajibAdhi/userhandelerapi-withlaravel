40ac2ef5f18b348ad649b8dc9a7a824917d7fc55
Sajib Adhikary <tosajibadhi@gmail.com>
2023-07-25 18:40:37 +0600

    refactor:  Api\UserController.php
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- app/Http/Controllers/Api/Admin/UserController.php
+++ app/Http/Controllers/Api/Admin/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UsersListRequest;
 use App\Http\Resources\UserResource;
-use App\Models\User;
 use App\Services\UserService;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
